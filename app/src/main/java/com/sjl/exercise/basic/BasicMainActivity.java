84d243854762f5bb9e2f7a11e915696a42ce18b7
林zero <1113225201@qq.com>
2019-06-16 17:21:07 +0800

    AppWidget

--- app/src/main/java/com/sjl/exercise/basic/BasicMainActivity.java
+++ app/src/main/java/com/sjl/exercise/basic/BasicMainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.sjl.exercise.base.BaseMainActivity;
 import com.sjl.exercise.basic.custom.CustomWidgetActivity;
+import com.sjl.exercise.basic.remote.AppWidgetProviderActivity;
 import com.sjl.exercise.basic.view.BasicViewActivity;
 import com.sjl.exercise.basic.widget.WidgetMainActivity;
 import com.sjl.exercise.bean.CatalogueBean;
@@ -22,5 +23,6 @@ public class BasicMainActivity extends BaseMainActivity {
         list.add(new CatalogueBean("控件", WidgetMainActivity.class));
         list.add(new CatalogueBean("自定义控件", CustomWidgetActivity.class));
         list.add(new CatalogueBean("View基础", BasicViewActivity.class));
+        list.add(new CatalogueBean("桌面小部件", AppWidgetProviderActivity.class));
     }
 }
---
+++ app/src/main/java/com/sjl/exercise/basic/remote/AppWidgetProviderActivity.java
@@ -0,0 +1,25 @@
+package com.sjl.exercise.basic.remote;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.sjl.exercise.R;
+import com.sjl.exercise.base.BaseActivity;
+
+public class AppWidgetProviderActivity extends BaseActivity {
+
+    @Override
+    public int getContentLayout() {
+        return R.layout.activity_app_widget_provider;
+    }
+
+    @Override
+    public void initView() {
+        findViewById(R.id.btn_update).setOnClickListener(v-> sendBroadcast(new Intent(MyAppWidget.ACTION_CLICK)));
+    }
+
+    @Override
+    public void initData(Bundle bundle) {
+
+    }
+}
